da2b3e55667bb905622b7b2b6e7da3e95e930948
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-02-24 11:37:46 -0300

    the same mistakes

---
+++ petcc_dados/dia1/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//dado um X, tal que existe Y em que Y - d(Y) = X, para arrays
+
+int main(){
+
+    //Quantidade de testes
+    unsigned int t;
+    cin >> t;
+
+    vector <int> vet;
+
+    
+
+
+
+
+
+    
+
+}
---
+++ training/bee1017.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int temp;
+    int vel;
+    int dis;
+    int l;
+
+    cin >> temp >> vel;
+    dis = temp * vel;
+
+    cout << dis << endl;
+
+    return 0;
+}
