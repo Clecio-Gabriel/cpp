8e66df8437ad89bea0b81460a77ca6bcfe6bb0ed
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-03-10 08:40:13 -0300

    starting problem in LP1

--- lp1/test.cpp
+++ lp1/test.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-
-using namespace std;
+#include <array>
 
 int main(){
 
+    std::array input {1, 5, 15, 7, 2, 6, 9, 3, -3, -5, 4};
+    //[1] Apresentar o vetor
+    std::cout << ">> input:[ " << std::endl;
+    for (const auto &element : input){ //const => ñ alterar | auto => deduzir tipo | & => pointer
+        std::cout << element << " ";
+    };
+    std::cout << "\n";
+    // [2] Percorrer para identificar o maior
+    //
+    // [3] Apresentar o maior e sua posição
     
-
     return 0;
 }
