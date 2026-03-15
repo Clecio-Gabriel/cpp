b2402c63c59c6613d37c416c3f49d125a726555c
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-03-15 02:15:19 -0300

    done with bee2787.cpp

---
+++ lp1/trabalho1/bee2787.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using u = unsigned;
+
+int blackorwhite(u line, u column);
+
+int main(){
+    u l{0};
+    u c{0};
+    int res{0};
+
+    std::cin >> l
+        >> c;
+    
+    res = blackorwhite(l,c);
+    
+    std::cout << res
+              << "\n";
+
+}
+
+int blackorwhite(u line, u column){
+    //It's just an IsEven function in disguise
+    u sum = line+column;
+    if (sum%2==0){
+        return 1;
+    }
+    return 0;
+}
