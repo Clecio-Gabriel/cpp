d8a2919c595c8190d82bd50db0b0ae523c245da8
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-03-30 08:13:48 -0300

    I'm legitimately going insane

--- lp1/trabalho1/bee1251.cpp
+++ lp1/trabalho1/bee1251.cpp
@@ -1,74 +1,116 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
 
 using string = std::string;
-using pairInt = std::pair <int,int>;
-using freq_map = std::map <pairInt,int>;
+using intPair = std::pair<int,int>;
+using intPairVector = std::vector <intPair>;
 
-int getFrequency(string inp, freq_map &res);
-int InvBubbleSort(string &in);
-int trade(char *a, char *b);
+int getFrequency(string &inp, intPairVector &res);  //WORKING
+template <typename typestr, typename type>  //WORKING
+void InsertionSort(typestr &structure, int end){
+
+    for (int i = 1; i < end; i++){
+        type key = structure[i];
+        int j = i - 1;
+        
+        while (j>=0 and structure[j] > key){
+            structure[j+1] = structure[j];
+            j--;
+        }
+        
+        structure[j+1] = key;
+        
+    }
+    
+}   
+int diffSort(intPairVector &input);
+int reverse(intPairVector &vec, int start_idx, int end_idx);
 
 int main(){
 
     string input {};
     
     while (std::getline(std::cin >> std::ws, input)){
-        InvBubbleSort(input); //To achieve the BIGGER ASCII restriction
-
-        freq_map result {};
+        
+        intPairVector result {};
         getFrequency(input, result);
-        for (auto element : result){
-            std::cout << element.second 
-                      << " "
-                      << element.first.first << "\n";
-        }
+        InsertionSort<intPairVector, intPair>(result, result.size());
+        diffSort(result);
+
         std::cout << "\n";
+        for (auto element : result){
+            std::cout << element.second
+                      << " "
+                      << element.first
+                      << "\n";
+        }
     }
 
     return 0;
 }
 
-int getFrequency(string inp, freq_map &res){
+int getFrequency(string &inp, intPairVector &res){
 
-    int inp_Size = inp.length();
-    int i{0};
-    int k{0};
-    while(i<inp_Size){
-        int ascii_code = inp[i];
-        int j = i+1;
-        int freq{0};
-        do{
-            freq++;
-        }while(inp[i++]==inp[j++]);
+    int freq{0};
 
-        res.insert(std::pair<pairInt,int>{pairInt{freq, k++},ascii_code});
+    while (true){
+        if (inp.empty()) break;
+        int inp_Size = inp.length();
+        int i{0};
+        int key = inp[i];
+        int ascii_code = key;
+        int j{inp_Size-1};
+        freq = 1;
+        while(j>i){
+            if (key==inp[j]) {
+                freq++;
+                inp.erase(inp.begin()+j);
+            };
+            j--;
+        };
+        inp.erase(inp.begin()+i);
+        i++;
+        
+        res.push_back(intPair {freq, ascii_code});
     }
 
     return 0;
 }
 
-int InvBubbleSort(string &in){
-
-    int size = in.length();
-    for (int i = size-1; i > 0; i--){
-        for (int j = size-1; j > 0; j--){
-            if (in[j]>in[j-1]){
-                trade(&in[j], &in[j-1]);
+int diffSort(intPairVector &input){
+    int size = input.size();
+    
+    int start{0};
+    int end{0};
+    if (size==1) return 0;
+    while (!(start>=size)){
+        for (int j = start; j < size; j++){
+            if (input[start].first==input[j].first){
+                end = j;
             }
         }
+        if(start!=end) reverse(input, start, end);
+        start = end+1;
     }
 
     return 0;
 }
 
-int trade(char *a, char *b){
+int reverse(intPairVector &vec, int start_idx, int end_idx){
+    intPairVector temp {};
     
-    int aux {0};
-    aux = *a;
-    *a = *b;
-    *b = aux;
+    int i = end_idx;
+    while (i>=start_idx){
+        temp.push_back(vec[i]);
+        i--;
+    }
+
+    int k{0};
+    for (int j = start_idx; j<=end_idx; j++){
+        vec[j] = temp[k];
+        k++;
+    }
 
     return 0;
 }
---
+++ training/insertionsort_template.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+template <typename typestr, typename type>
+void InsertionSort(typestr &structure, int end, char op){
+
+    for (int i = 1; i < end; i++){
+        type key = structure[i];
+        int j = i - 1;
+        
+        switch(op){
+            case 'a':
+                while (j>=0 and structure[j] > key){
+                    structure[j+1] = structure[j];
+                    j--;
+                }
+                break;
+            case 'd':
+                while (j>=0 and structure[j] < key){
+                    structure[j+1] = structure[j];
+                    j--;
+                }
+        }
+
+        structure[j+1] = key;
+        
+    }
+    
+}
+
+template <typename T>
+void printVet(std::vector<T> l){
+    for (auto el : l){
+        std::cout << el << " ";
+    }
+    std::cout << "\n";
+}
+
+void printPair(std::vector<std::pair<int,int>> l){
+    for (auto el : l){
+        std::cout << el.first << "-" << el.second << " ";
+    }
+    std::cout << "\n";
+}
+
+int main(){
+
+    std::vector inp {8,7,9,0,3,2,55,43,23,33};
+
+        std::cout << "COM INTEIROS:\n\nLISTA ORIGINAL:\n"; 
+        printVet<int>(inp);
+
+        std::cout <<"Ordenamento Normal: \n";
+        InsertionSort<std::vector<int>, int>(inp, 10, 'a');
+        printVet<int>(inp);
+
+        std::cout << "Reverse: \n";
+        InsertionSort<std::vector<int>, int>(inp, 10, 'd');
+        printVet<int>(inp);
+
+    std::vector <double> inp2 = {1.2, 4.5, 6.7, 8.9, 90.0, 20.0, 30.0, 34.5, 85.3, 23.55, 123.2, 48.0};
+    std::cout << "\nCOM DOUBLE:\n\nLISTA ORIGINAL:\n";
+    printVet<double>(inp2);
+    
+        std::cout << "Ordenamento Normal:\n";
+        InsertionSort<std::vector<double>, double>(inp2, inp2.size(), 'a');
+        printVet<double>(inp2);
+
+        std::cout << "Reverse:\n";
+        InsertionSort<std::vector<double>, double>(inp2, inp2.size(), 'd');
+        printVet<double>(inp2);
+
+    std::cout << "\nCOM PARES DE INTEIROS:\n\nLISTA ORIGINAL\n";
+    std::vector <std::pair<int,int>> vetpair {{2,5},{2,3},{3,9},{3,8},{4,90},{23,7}, {1,0}};
+    printPair(vetpair);
+
+        std::cout << "Ordenamento Normal:\n";
+        InsertionSort<std::vector<std::pair<int,int>>, std::pair<int,int>>(vetpair, vetpair.size(), 'a');
+        printPair(vetpair);
+    
+        std::cout << "Reverse:\n";
+        InsertionSort<std::vector<std::pair<int,int>>, std::pair<int,int>>(vetpair, vetpair.size(), 'd');
+        printPair(vetpair);
+    
+    return 0;
+}
